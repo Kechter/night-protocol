599afea42acc4b3ba9e69d146b48168bd7e59e06
Kechter <joni.kechter@gmail.com>
2026-03-12 18:49:55 +0100

    fix settings

--- src/utils/Config.js
+++ src/utils/Config.js
@@ -2,9 +2,9 @@ export const Config = {
   width: 1920,
   height: 1080,
   zoomFactor: 2,
-  debug: true,
-  adminMode: true, // true = alle Keys erhalten beim Start
-  skipMinigames: true, // true = Minigames auto-complete
+  debug: false,
+  adminMode: false, // true = alle Keys erhalten beim Start
+  skipMinigames: false, // true = Minigames auto-complete
   difficulty: "normal", // 'easy' | 'normal' | 'hard' | 'hardcore' (überschrieben von DifficultySelectScene)
   physics: {
     gravity: 0,
